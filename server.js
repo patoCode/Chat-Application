07d298acc07c99a52306d59f8695901478cccf2c
Kyle Lim <kylelim8998@gmail.com>
2017-10-07 22:41:48 -0400

    Working Chat With Sockets
    Users can now log into the chat app and send messages!

--- server.js
+++ server.js
@@ -18,6 +18,29 @@ io.sockets.on('connection', function(socket){
     console.log('Connected: %s sockets connected', connections.length);
 
     // Disconnect
-    connections.splice(conections.indexOf(socket), 1);
-    console.log('Disconnected: %s sockets connected', connections.length);
+    socket.on('disconnect', function(data){
+        // if(!socket.username) return;
+        users.splice(users.indexOf(socket.username), 1);
+        updateUsernames();
+        connections.splice(connections.indexOf(socket), 1);
+        console.log('Disconnected: %s sockets connected', connections.length);
+    });
+
+    // Send Message
+    socket.on('send message', function(data){
+        // console.log(data);
+        io.sockets.emit('new message', {msg: data, user: socket.username});
+    });
+
+    // New User
+    socket.on('new user', function(data, callback){
+        callback(true);
+        socket.username = data;
+        users.push(socket.username);
+        updateUsernames();
+    });
+
+    function updateUsernames(){
+        io.sockets.emit('get users', users);
+    }
 });
